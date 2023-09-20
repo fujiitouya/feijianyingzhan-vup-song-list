1413657ff8dcfea0718a8dbd314fa7a5a7974ebc
kazusalove <a6087349@outlook.com>
2023-09-20 19:43:55 +0800

    update for the next.config.js..... if works.

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   trailingSlash: true,
   exportPathMap: function () {
     return {
-      '/': {page: "/"},
+      '/feijianyingzhan-vup-song-list': {page: "/feijianyingzhan-vup-song-list"},
       '/404': {page: "/404"}
     }
   },
