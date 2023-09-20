6b4bb226f58e03b1717fccfa693152ed128845c6
wuyanguo <48616819+chungkamyin@users.noreply.github.com>
2023-09-20 15:06:45 +0800

    migrate code to this repository
    migrate code to this repository

---
+++ config/constants.js
@@ -0,0 +1,35 @@
+let config = {
+    Name: "绯间樱斩",
+    
+    BiliLiveRoomID: "30472608",
+    
+    NetEaseMusicId: "",
+    QQMusicId: "",
+    Footer: "Copyright © 2023 绯间樱斩 解释权归本直播间所有。",
+
+    Cursor: true,
+
+    LanguageCategories: ["国语", "日语",],
+    RemarkCategories: [],
+
+    BannerTitle: "雪域provealms所属，一名在浮空之城游荡的侦探",
+    BannerContent: [``,],
+
+    CustomButtons: [
+        {
+            link: "mailto:provealms@foxmail.com",
+            name: "商务合作",
+            image: ""
+        },
+        {
+            link: "https://space.bilibili.com/5221643",
+            name: "录播组",
+            image: ""
+        }
+    ]
+}
+
+
+module.exports = {
+    config
+}
